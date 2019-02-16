e35aa251a6d4d34c4f8425127721151ab0623f0a
Richard <accounts+github@richardyang.ca>
2019-02-16 16:45:23 -0500

    Revert "2018d1 optimize"
    This reverts commit ba875c599b1c2eac8116c5f74c2d443307177cc6.

--- 2018D1/sum.py
+++ 2018D1/sum.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python
 
 filepath = 'input.txt'
-from datetime import datetime
-startTime = datetime.now()
 
 def main():
     nums = []
     sums = {0}
-    result1, currentSum = 0, 0
-    firstIteration = True
+    result1, result2, currentSum = 0, 0, 0
+    stillSearching, firstIteration = True, True
     
+
     with open(filepath) as f:
         for line in f.readlines():
             nums.append(int(line))
         f.close()
 
-    while(True):
+    while(stillSearching):
         for val in nums:
             currentSum += int(val)
 
-            if  currentSum in sums:
-                return result1, currentSum
-           
+            if stillSearching and currentSum in sums:
+                result2 = currentSum
+                stillSearching = False
             sums.add(currentSum)
 
         if firstIteration:
             result1 = currentSum
             firstIteration = False
         
+    return result1, result2
+
 if __name__ == '__main__':
-    print(main(), str(datetime.now() - startTime)[:-3])
+    result = main()
+    print("{},{}".format(result[0], result[1]))
